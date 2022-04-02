d0ce8eed8a8f4a44e2c7198d23fa56e7428f46c9
Nicolas Peralta <nicolasperalta27@gmail.com>
2022-04-01 23:07:20 -0400

    upload portfolio

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+/*
+	Directive by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body');
+
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '1281px',  '1680px' ],
+			normal:    [ '981px',   '1280px' ],
+			narrow:    [ '841px',   '980px'  ],
+			narrower:  [ '737px',   '840px'  ],
+			mobile:    [ '481px',   '736px'  ],
+			mobilep:   [ null,      '480px'  ]
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+
+})(jQuery);
+
+
+const freelance = document.querySelector('#freelance')
+const career = document.querySelector('#career')
+
+
+document.querySelector("#freelanceButton").addEventListener("click",showFreelance)
+document.querySelector("#careerButton").addEventListener("click",showCareer)
+
+
+function showFreelance (){
+	career.classList.add('hiddenCareer')
+	freelance.classList.toggle('hiddenFreelance')
+}
+
+function showCareer (){
+	freelance.classList.add('hiddenFreelance')
+	career.classList.toggle('hiddenCareer')
+}
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
